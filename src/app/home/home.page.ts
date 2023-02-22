377da1ae746e7992f219392efa76d521bf28b3d1
vicky435435 <vickymaneshwar435@gmail.com>
2023-02-22 20:55:54 +0530

    ionic news app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {SwiperComponent} from "swiper/angular";
+
+// import Swiper core and required modules
+import SwiperCore, {EffectCards}from "swiper";
+
+// install swiper modules
+SwiperCore.use([EffectCards]);
 
 @Component({
   selector: 'app-home',
@@ -7,6 +14,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  show = true;
 
+share() {
+console.log('hellow')
+}
+
+  constructor() {}
+  hide(){
+     this.show = !this.show;
+  }
 }
